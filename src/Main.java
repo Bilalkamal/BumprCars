56b6f1acfc6f7567ebe1119f427e7f931feba772
Bilal K. Hamada <bilalkamal2009@gmail.com>
2020-09-13 18:59:47 -0500

    Created File reader method to read .dat files

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,55 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		File  personsFile = new File("data/Persons.dat");
+	
+		File  customersFile = new File("data/Customers.dat");
+		File  productFile = new File("data/Products.dat");
+		
+		
+		readFile(personsFile);
+		System.out.println("\n" + "\n"+ "****************************************************************" +  "\n");
+		
+		readFile(customersFile);
+		System.out.println("\n" + "\n"+ "****************************************************************" +  "\n");
+		readFile(productFile);
+		System.out.println("\n" + "\n"+ "****************************************************************" +  "\n");
+		
+	}
+
+	
+	
+//	Scanner function
+//	TO-DO:
+//	Add returning array of lines
+	
+	public static void readFile(File file) {
+
+
+		try (FileInputStream fis = new FileInputStream(file)) {
+ 			int content;
+			while ((content = fis.read()) != -1) {
+				// convert to char and display it
+				System.out.print((char) content);
+			} 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	
+		
+		
 		
 	}
 
