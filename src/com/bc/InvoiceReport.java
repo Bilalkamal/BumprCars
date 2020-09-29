78c34fbde93befaa755ec22164025b9118afccaf
Bilal K. Hamada <bilalkamal2009@gmail.com>
2020-09-29 10:34:29 -0500

    Beginning work on Invoice Report.

---
+++ src/com/bc/InvoiceReport.java
@@ -0,0 +1,20 @@
+package com.bc;
+
+import java.util.ArrayList;
+
+public class InvoiceReport extends ParserFunctions {
+
+//	Done --> TO-DO: Create an Invoice Class - with the required specs 
+	
+//	Done --> TO-DO: Parser function for Invoices.dat
+//				This should be done on the parser functions class
+//	TO-DO: Report Functions for the Invoices 
+//	TO-DO: Calling the System Report from Data converter 
+
+	
+	
+	
+	
+	
+	
+}
