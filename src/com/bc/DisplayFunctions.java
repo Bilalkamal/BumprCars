57cf5b2b42e04bfdef6bdfbbfdf469a09b054a8a
Bilal K. Hamada <bilalkamal2009@gmail.com>
2020-10-01 23:52:29 -0500

    Created the first part of the detailed invoice (Owner & Customer).

--- src/com/bc/DisplayFunctions.java
+++ src/com/bc/DisplayFunctions.java
@@ -1,7 +1,9 @@
 package com.bc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class DisplayFunctions {
 
@@ -25,7 +27,7 @@ public class DisplayFunctions {
 
 		for (Invoice s : lInv) {
 
-//			TO-DO: Convert these into functions and just call them here.
+//			TODO: Convert these into functions and just call them here.
 
 //			Check Ownercode
 
@@ -33,7 +35,7 @@ public class DisplayFunctions {
 
 			String ownerName = null;
 
-			List<Double> subtotalResult = calculateSubtotal(lprod, s.getListOfProducts());
+			List<Double> subtotalResult = calculateInvoiceSubtotal(lprod, s.getListOfProducts());
 			Double itemSubtotal = Math.round(subtotalResult.get(0) * 100.0) / 100.0;
 			Double itemDiscount = Math.round(subtotalResult.get(1) * 100.0) / 100.0;
 
@@ -43,7 +45,7 @@ public class DisplayFunctions {
 
 			for (Person p : lpers) {
 				if (p.getPersonCode().equals(ownerCode)) {
-					ownerName = p.getFirstName() + " " + p.getLastName();
+					ownerName = p.getFirstName() + ", " + p.getLastName();
 				}
 
 			}
@@ -100,29 +102,111 @@ public class DisplayFunctions {
 
 	}
 
-	
-//	Invoise repot function
+//	Invoice detailed report function
 	public static void invoiceReport(List<Invoice> lInv, List<Customer> lc, ArrayList<Person> lpers,
 			List<Product> lprod) {
-		
 
-		System.out.println("Invoice Details: \n =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+" );
-		
-		
+		System.out.println(
+				"Invoice Details: \n =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+");
+
+		for (Invoice inv : lInv) {
+
+			System.out.println("Invoice " + inv.getInvoiceCode() + "\n--------------------------------------");
+			System.out.println("Owner: \n");
+
+			String ownerCode = inv.getOwnerCode();
+			String ownerName = null;
+			EmailAddress emails = null;
+			Address address = null;
+
+			for (Person p : lpers) {
+				if (p.getPersonCode().equals(ownerCode)) {
+					ownerName = p.getLastName() + ", " + p.getFirstName();
+					emails = p.getEmailAddress();
+					address = p.getAddress();
+
+				}
+
+			}
+
+//			TODO: Fix Invoice 6 issue with emails  
+//			TODO: Fix spacing Issues 
+
+			System.out.printf("%20s ", ownerName + "\n");
+
+//			,   (emails.getEmailAddress().isEmpty() ? "[]" : emails.getEmailAddress())
+
+			System.out.println(address.getStreet() + "\n" + address.getCity() + "," + address.getState()
+					+ address.getZip() + " " + address.getCountry() + " \n");
+
+//			Customer data
+			System.out.println("Customer:");
+			String customerCode = inv.getCustomerCode();
+
+			String customerName = null;
+
+			for (Customer c : lc) {
+				if (c.getCustomerCode().equals(customerCode)) {
+
+					if (c.getCustomerType() == "B") {
+						customerName = c.getName();
+					} else {
+						customerName = c.getName();
+					}
+
+					address = c.getAddress();
+
+				}
+
+			}
+
+			System.out.printf("%3s %s ", customerName, "\n");
+			System.out.println(address.getStreet() + "\n" + address.getCity() + "," + address.getState()
+					+ address.getZip() + " " + address.getCountry() + " \n");
+
+//			Products Part
+			System.out.println("Products: \n");
+			
+			
+			System.out.println(
+					"Code			Dicription				                 		Subtotal		Discount		Fees			Taxes			Total");
+
+			System.out.println(
+					"-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+
+			String productCode = null;
+			
+			
+			
+			
+			
+//			TODO: Fix Spacing for Thanks
+			System.out.printf("%87s %33s", " \n \n THANK YOU FOR DOING BUSINESS WITH US!  \n \n \n \n", "\n");
+			System.out.println("+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=");
+			
+
+		}
+
 	}
-	
-//	Subtotal and Discounts function
-	
-	public static List<Double> calculateSubtotal(List<Product> productList, List<String> boughtList) {
 
-//		0th element is the subtotal
+//	Sub-total and Discounts function
+	public static List<Double> calculateInvoiceSubtotal(List<Product> productList, List<String> boughtList) {
+
+//		0th element is the sub-total
 		Double subTotal = 0.0;
 		Double itemCost = 0.0;
 //		1st element is the concession discount 
 		Double concessionTotalDiscount = 0.0;
 		Double concessionDiscount = 0.0;
 
-//		3rd element is the taxes 
+		Map<String, Integer> itemsInInvoice = new HashMap<String, Integer>();
+
+		itemsInInvoice.put("Towing", 0);
+		itemsInInvoice.put("Repair", 0);
+		itemsInInvoice.put("Rental", 0);
+//		Although we're not using concession for a discount, 
+//		we might needed it any time else so it would be better to have it here.
+		itemsInInvoice.put("Concession", 0);
 
 		List<Repair> lrep = new ArrayList<Repair>();
 
@@ -143,12 +227,20 @@ public class DisplayFunctions {
 							itemCost = r.getRentCost();
 
 							subTotal += itemCost;
+							itemsInInvoice.put("Rental", itemsInInvoice.get("Rental") + 1);
 
 						} else if (p.getProductType().equals("T")) {
 							Towing t = new Towing((Towing) p, Double.parseDouble(itemTokens[1]));
 							t.setMilesTowed(Double.parseDouble(itemTokens[1]));
 							itemCost = t.getTowingcost();
 							subTotal += itemCost;
+							itemsInInvoice.put("Towing", itemsInInvoice.get("Towing") + 1);
+
+							if ((itemsInInvoice.get("Towing") > 0) && (itemsInInvoice.get("Rental") > 0)
+									&& (itemsInInvoice.get("Repair") > 0)) {
+								subTotal -= itemCost;
+
+							}
 
 						} else if (p.getProductType().equals("F")) {
 							Repair f = new Repair((Repair) p, Double.parseDouble(itemTokens[1]));
@@ -157,12 +249,14 @@ public class DisplayFunctions {
 							subTotal += itemCost;
 
 							lrep.add(f);
+							itemsInInvoice.put("Repair", itemsInInvoice.get("Repair") + 1);
 
 						} else if (p.getProductType().equals("C")) {
 							Concession c = new Concession((Concession) p, Integer.parseInt(itemTokens[1]));
 							c.setQuantity(Integer.parseInt(itemTokens[1]));
 							itemCost = c.getConcessionCost();
 							subTotal += itemCost;
+							itemsInInvoice.put("Concession", itemsInInvoice.get("Concession") + 1);
 						}
 
 					}
@@ -180,6 +274,7 @@ public class DisplayFunctions {
 
 						concessionDiscount = itemCost * -0.1;
 						concessionTotalDiscount += concessionDiscount;
+						itemsInInvoice.put("Concession", itemsInInvoice.get("Concession") + 1);
 
 					}
 
